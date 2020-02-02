2c125e39bb7a8d01386915b50363e06424a8d13b
Casneil Simpson <casneil.simpson@gmail.com>
2020-02-02 12:53:16 +0100

    adding .env variables

--- server/server.js
+++ server/server.js
@@ -1,9 +1,19 @@
 const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
+require("dotenv").config({ path: "../.env" });
 
 const port = process.env.Port || 8080;
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+const uri = process.env.ATLAS_URI;
+
+mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+const connection = mongoose.connection;
+connection.once("open", () => console.log("MongoDB connected successfully!"));
+
+app.listen(port, () => console.log(`Listening on port${port}`));
