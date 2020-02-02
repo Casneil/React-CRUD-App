8c6cc7dd4906748aa8754ca854d710343e5bab0e
Casneil Simpson <casneil.simpson@gmail.com>
2020-02-02 15:14:11 +0100

    adding crud operations

--- routes/articles.js
+++ routes/articles.js
@@ -2,10 +2,49 @@ const express = require("express");
 const router = express.Router();
 const Articles = require("../schemas/articles");
 
+/****************Request to get all articles*****************/
 router.get("/", (req, res) => {
   Articles.find()
     .then(article => res.json(article))
-    .catch(error => res.status(400).res.json(`Error: ${error}`));
+    .catch(error => res.status(400).json(`Error: ${error}`));
 });
+
+/****************Request to find articles by id*****************/
+router.get("/:id", (req, res) => {
+  Articles.findById(req.params.id)
+    .then(article => res.json(article))
+    .catch(error => res.status(400).json(`Error: ${error}`));
+});
+
+/****************Request to add new articles*****************/
+router.post("/add", (req, res) => {
+  const newArticle = new Articles({
+    title: req.body.title,
+    article: req.body.article,
+    authorName: req.body.authorName
+  });
+
+  newArticle
+    .save()
+    .then(() => res.json("Article posted!"))
+    .catch(error => res.status(400).json(`Error: ${error}`));
+});
+
+/****************Request to find articles by id and update*****************/
+router.put("/update/:id", (req, res) => {
+  Articles.findById(req.params.id)
+    .then(article => {
+      (article.title = req.body.title),
+        (article.article = req.body.article),
+        (article.authorName = req.body.authorName);
+      article
+        .save()
+        .then(() => res.json("Article updated!"))
+        .catch(error => res.status(400).json(`Error:${error}`));
+    })
+    .catch(error => res.status(400).json(`Error:${error}`));
+});
+
+/****************Request to find articles by id and delete*****************/
 
 module.exports = router;
